de9a2620b2c3d41e070d6947962f689f2568595a
EmiKnape <enknape@aol.com>
2018-06-08 17:55:29 +0000

    automatically backed up by learn

--- emi_closet.rb
+++ emi_closet.rb
@@ -1,3 +1,6 @@
-closet= [tops 
-
-]
+closet= [
+  tops= ["blouse", "sweater","tshirts", "croptops"]
+  bottoms=["jeans", "sweatpants", "shorts", "jorts", "skirts", ]
+  footwear=["running", "sandals", "boots", "flipflops", "converse"]
+  ]
+  puts footwear
